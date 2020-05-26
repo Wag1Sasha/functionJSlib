0badac0eb1084e5a5b2cb9cb8b0da95a1be151ce
Wag1Sasha <4goenko@gmail.com>
2020-05-26 16:15:49 +0300

    function find && tests

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const map = require("./lib/map");
 const reduceNumbers = require("./lib/reduceNumbers");
 const filter = require("./lib/filter");
+const find = require("./lib/find");
 
 const arr = [1, 5, 2, 3, 15, 22, 31];
 console.log(map(arr, (item) => item + 1));
 console.log(reduceNumbers(arr, reducer));
 console.log(filter(arr, filtred));
+console.log(find(arr, findNumber));
+
+function findNumber(elem) {
+  return elem < 3 ;
+}
 
 function reducer(acc, currentValue) {
   return acc + currentValue;
---
+++ tests/find.test.js
@@ -0,0 +1,26 @@
+const find = require("../lib/find");
+
+test("find first number < 5 :", () => {
+  const arr = [1, 5, 2, 3, 15, 22, 31];
+  function findNumber(elem) {
+    return elem < 3 ;
+  }
+  expect(find(arr, findNumber)).toEqual(arr.find(findNumber));
+});
+
+
+test("find first number < -3 :", () => {
+  const arr = [1, 5, 2, 3, 15, 22, 31];
+  function findNumber(elem) {
+    return elem < -3 ;
+  }
+  expect(find(arr, findNumber)).toEqual(arr.find(findNumber));
+});
+
+test("find first string with length > 5 :", () => {
+  const arr = ['Kenye','Robin','Admiral','Marshal'];
+  function findNumber(elem) {
+    return elem < -3 ;
+  }
+  expect(find(arr, findNumber)).toEqual(arr.find(findNumber));
+});
